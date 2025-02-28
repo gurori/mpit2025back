270f08f6573d074182270a0bbbb70a372c3e8709
Gurori <nazarovslava124@gmail.com>
2025-02-28 11:58:16 +0900

    add docker

--- mpit/Program.cs
+++ mpit/Program.cs
@@ -1,23 +1,62 @@
+using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+var config = builder.Configuration;
 
 // Add services to the container.
 
-builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+services.AddCors(o => {
+    o.AddDefaultPolicy(p => {
+        p.AllowAnyHeader();
+        p.AllowAnyMethod();
+        p.AllowCredentials();
+        p.WithOrigins("http://localhost:3000", "https://localhost:3000");
+    });
+});
+
+services.AddControllers();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+// Add DI (Services, Mapping, DbContext)
+
+services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseNpgsql(config.GetConnectionString(nameof(ApplicationDbContext))));
 
 var app = builder.Build();
+
+
+using var scope = app.Services.CreateScope();
+await using var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+await dbContext.Database.EnsureCreatedAsync();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
 
-app.UseAuthorization();
+app.UseCookiePolicy(new CookiePolicyOptions
+{
+    MinimumSameSitePolicy = SameSiteMode.Strict,
+    HttpOnly = HttpOnlyPolicy.Always,
+    Secure = CookieSecurePolicy.Always,
+});
 
 app.MapControllers();
+
+app.UseCors();
+
+app.UseAuthorization();
 
 app.Run();
